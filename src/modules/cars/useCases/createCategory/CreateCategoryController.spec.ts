3a67d751aef55efff2883f5ea3d11d259d070325
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-08-02 15:56:52 -0300

    Update repo

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,10 +1,42 @@
+import { hash } from "bcryptjs";
 import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
 
 import { app } from "@shared/infra/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
 
 describe("Create category controlle", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidv4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", driver_license, created_at)
+    values('${id}', 'admin', 'admin@admin.com', '${password}', true, 'XXXXX', 'now()')
+    `
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
   it("Should be able to create a new category", async () => {
-    const response = await request(app).get("/categories").send({
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@admin.com",
+      password: "admin",
+    });
+
+    console.log(responseToken.body);
+
+    const response = await request(app).post("/categories").send({
       name: "Category SUPERTEST",
       description: "Categories in the supertest",
     });
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,7 @@ export default async (host = "database"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "teste" ? "localhost" : host,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
